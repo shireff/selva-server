09063211c0e2a9bec106770588b6e02f92794f3c
Shireff nady <shireffn369@gmail.com>
2025-06-20 02:43:19 +0300

    update CORS origin to use production URL

--- src/server.js
+++ src/server.js
@@ -59,7 +59,7 @@ const swaggerSpec = swaggerJsdoc(swaggerOptions);
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: "https://selva-nail-shop.vercel.app" || 'http://localhost:5173',
   credentials: true
 }));
 
